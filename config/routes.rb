ea4e39a096bfa7d2fa3421775336ebb71d1bf70d
Tobu Widjaja <tobuwidjaja@gmail.com>
2020-03-04 20:43:42 -0500

    changes to views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do 
 	resources :layouts
 	get 'layouts/landing' #receives incoming request for layouts.ladning
-	root 'layouts#landing' #the root page will be layouts.;admgon 	
+	root'layouts#landing' #the root page will be layouts.;admgon 	
+
 	get '/login', to: 'layouts#login' 
 	get '/register', to: 'layouts#register'
+	get '/login/register', to: 'layouts#register'
 	
 end 
