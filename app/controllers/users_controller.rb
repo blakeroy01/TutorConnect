e78dfc38bf002a2d144bafe8a57a16d8a82710fa
blakeroy01 <44304480+blakeroy01@users.noreply.github.com>
2020-03-11 03:51:38 -0400

    Overnight

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,12 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/dashboard'
+      redirect_to '/pre_dashboard'
     else
       flash[:register_errors] = user.errors.full_messages
-      redirect_to '/'
+      render 'users/pop_up'
     end
   end
-
 
   private
     def user_params
