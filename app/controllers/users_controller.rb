42b78e0038055b103a3bd8bc625a1972d3e7fcc0
Kyle Welch <kywelch17@gmail.com>
2020-04-03 15:52:41 -0400

    Added CRUD functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,50 @@
 class UsersController < ApplicationController
+  #made
+  def index
+    @user = User.all  
+  end
 
   def create
-      user = User.new(user_params)
+    user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
       redirect_to "/pre_dashboard#{user_params[:is_tutor]}"
     else
-      flash[:register_errors] = @current_user.errors.full_messages
+      flash[:register_errors] = 'User exists'
     end
+  end
+
+  #made  
+  def new
+    @user = User.new
+  end
+
+  #made
+  def edit
+    @user = User.find(user_params[:id])
+  end
+
+  #made
+  def show
+    @user = User.find(user_params[:id])
+  end
+
+  #made
+  def update
+    @user = User.find(user_params[:id])
+    if @user.update
+      redirect_to @user, notice: 'Updated!'
+    else
+      render 'edit'
+    end
+  end
+
+  #made
+  def destroy
+    @user = User.find(user_params[:id])
+    @user.destroy
+
+    redirect_to root_url, notice: 'Deleted'
   end
 
   private
