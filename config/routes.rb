37b09d90230c0b7c3b55d5c7e84482a535ff85b5
Kyle Welch <kywelch17@gmail.com>
2020-04-21 12:58:28 -0400

    Edit and delete done.

--- config/routes.rb
+++ config/routes.rb
@@ -12,14 +12,16 @@ Rails.application.routes.draw do
 	get '/register' => 'layouts#register'
 	get '/dashboard' => 'dashboard#index'
 	#edit_profile true false represents the edit profile page for tutors and students
+	get '/pre_dashboardfalse' => 'dashboard#pre_index'
+	get '/pre_dashboardtrue' => 'dashboard#pre_index_tutor'
 	get '/edit_profilefalse' => 'users#edit'
 	get '/edit_profiletrue' => 'users#edit_tutor'
 	#pre_dashboard true false represent the pre_dashboard page for tutors and students
-	get '/pre_dashboardfalse' => 'dashboard#pre_index'
-	get '/pre_dashboardtrue' => 'dashboard#pre_index_tutor'
 	post '/dashboard_update_bio' => 'dashboard#update_bio'
 	post '/dashboard_update_bio_subjects' => 'dashboard#update_bio_subjects'
 	 get 'logout', to: 'sessions#destroy', as: 'logout'  
 	get '/settings' => 'layouts#settings'
+	get '/tutors' => 'layouts#index'
+	get '/delete' => 'users#destroy'
 
 end
