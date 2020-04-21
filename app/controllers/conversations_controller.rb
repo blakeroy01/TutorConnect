c0ff7b9c78565a047c7b49295ebe38f17f867ae0
blakeroy01 <44304480+blakeroy01@users.noreply.github.com>
2020-04-21 13:35:54 -0400

    Merged develop

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,29 +1,6 @@
 class ConversationsController < ApplicationController
 
-  def show
-    render ''
-  end
+def create
 
-  def create
-    @tutor = current_user
-    @student = User.find_by(id: requesting_student.id)
-    @conversation = Conversation.create(
-      tutor_id: ,
-      second_user_id: params[:second_user_id]
-    )
-  end
-
-  def create_request
-    current_user.add_request
-  end
-
-  def requesting_student
-    @requesting_student = User.find_by
-  end
-
-  private
-
-  def conversation_params
-    params.permit(:username, :id)
-  end
+end
 end
