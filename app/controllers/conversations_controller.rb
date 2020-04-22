a658e1cf16104d2597e4b3ed117b5487427a58f7
blakeroy01 <44304480+blakeroy01@users.noreply.github.com>
2020-04-22 18:09:28 -0400

    messaging backend

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,9 +1,5 @@
 class ConversationsController < ApplicationController
-
-  def show
-    conversation_messages = Message.where(conversation_id: this_id)
-  end
-
+  
   def delete
     conversation = Conversation.find_by(id: id)
     conversation.destroy
